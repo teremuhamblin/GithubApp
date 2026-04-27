f2e81588b36a2d9e4f07e76de2f58cb0bbbd89d2
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-04-27 22:43:45 +0200

    Update github.js

--- src/github.js
+++ src/github.js
@@ -1,1 +1,13 @@
+import { Webhooks } from "@octokit/webhooks";
+import { Octokit } from "@octokit/core";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+export const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN
+});
+
+export const webhooks = new Webhooks({
+  secret: process.env.WEBHOOK_SECRET
+});
