4afb9f153b5abaa7dd9c2351abf8f2942d398d84
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-05-08 23:32:02 +0200

    Create index.js

---
+++ src/handlers/index.js
@@ -0,0 +1,15 @@
+import issuesReopened from "./issuesReopened.js";
+import pullRequestReview from "./pullRequestReview.js";
+import pullRequestMerged from "./pullRequestMerged.js";
+import commentEdited from "./commentEdited.js";
+import autoLabels from "./autoLabels.js";
+import autoReply from "./autoReply.js";
+
+export default {
+  issuesReopened,
+  pullRequestReview,
+  pullRequestMerged,
+  commentEdited,
+  autoLabels,
+  autoReply
+};
